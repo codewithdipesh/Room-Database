590d262c5ef2431170defaaa84de5d5ecc3e7b5e
Dipesh Pal <codewithdipesh2022@gmail.com>
2024-05-23 23:55:11 +0530

    fixed padding error

--- app/src/main/java/com/example/flows/ContactScreen.kt
+++ app/src/main/java/com/example/flows/ContactScreen.kt
@@ -1,10 +1,12 @@
 package com.example.flows
 
 import android.widget.ImageButton
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -26,6 +28,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -50,15 +53,19 @@ fun ContactScreen(
             }
 
         },
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier
+
     ) {padding ->
         if(state.isAddingContact){
             AddDialog(state = state, onEvent = onEvent )
         }
         LazyColumn(
             contentPadding = padding,
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
+
         ) {
             item {
                 Row (
